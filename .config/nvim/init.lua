e1cad1cd3a1f6a4db5029f10afe009d498ab0a34
Myouboku <71690611+Myouboku@users.noreply.github.com>
2024-11-17 15:25:46 +0100

    web dev lsp, linter and formater

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -567,8 +567,10 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
+        ts_ls = {},
+        jsonls = {},
+        html = {},
+        cssls = {},
 
         bashls = {
           filetypes = { 'sh', 'zsh' },
@@ -609,6 +611,9 @@ require('lazy').setup({
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'beautysh',
+        'eslint_d',
+        'prettier',
+        'prettierd',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
