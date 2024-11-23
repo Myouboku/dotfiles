264afe2735adad1b73e79e7bf1f105bb7db2e3cd
Myouboku <71690611+Myouboku@users.noreply.github.com>
2024-11-23 16:06:11 +0100

    improved unitstatus command

--- .zsh.d/unitstatus.sh
+++ .zsh.d/unitstatus.sh
@@ -1,6 +1,6 @@
 function unitstatus() {
     local unit
-    unit=$(systemctl list-units --no-pager --plain | fzf | awk '{print $1}')
+    unit=$(systemctl list-unit-files --no-pager --plain | sed '1d' | fzf --layout=reverse | awk '{print $1}')
 
     if [[ -n "$unit" ]]; then
         systemctl status "$unit"
