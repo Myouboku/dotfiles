6eba8ffb14a017b189ce16bd69ac2cc59c165504
Hugo Avril <96306153+HugoAVRIL@users.noreply.github.com>
2025-05-21 16:38:29 +0200

    fix: alias node

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -2,8 +2,9 @@ alias lg='lazygit'
 alias port-forwarding="\$HOME/scripts/port_forwarding.sh"
 alias unitstatus="\$HOME/scripts/unitstatus.sh"
 alias cu="\$HOME/scripts/checkupdates.sh"
-alias devm='NODE_ENV=development nodemon --inspect -e js,cjs,mjs,sql src/index.js'
-alias dev='NODE_ENV=development node --inspect src/index.js'
+alias up="flatpak update && yay"
+alias devm='cd srv && NODE_ENV=development nodemon --inspect -e js,cjs,mjs,sql src/index.js'
+alias dev='cd srv && NODE_ENV=development node --inspect src/index.js'
 alias testapi='NODE_ENV=test npm run pretest:api && npx mocha --parallel --exit'
 alias test='NODE_ENV=test npx mocha --parallel --exit'
 alias bright='systemctl --user restart plasma-powerdevil.service'
