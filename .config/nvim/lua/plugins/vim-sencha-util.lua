fcfed4f0d41a08060803965e32339d23139de59c
Hugo Avril <96306153+HugoAVRIL@users.noreply.github.com>
2025-05-22 18:49:34 +0200

    feat: lazyvim keymaps and vim sencha (kinda)

---
+++ .config/nvim/lua/plugins/vim-sencha-util.lua
@@ -0,0 +1,7 @@
+return {
+  "tnker/vim-sencha-util",
+  keys = {
+    { "<leader>vt", "<cmd>SenchaMVVMToggle<cr>", desc = "Sencha MVVM Toggle" },
+    { "<leader>vg", "<cmd>SenchaParserReadCurrentLine<cr>", desc = "Sencha Go To" },
+  },
+}
