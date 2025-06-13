064ff3d54eeecc7cf0ad7aa4a5bdd40c585f0464
Hugo Avril <96306153+HugoAVRIL@users.noreply.github.com>
2025-06-13 16:49:22 +0200

    feat: nvim theme toggle

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -7,6 +7,9 @@ return {
       italic_comments = true,
       borderless_pickers = true,
     },
+    keys = {
+      { "<leader>ut", "<cmd>CyberdreamToggleMode<CR>", desc = "Toggle Light/Dark theme" },
+    },
   },
   {
     "LazyVim/LazyVim",
