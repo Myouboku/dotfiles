9db47e407b319b4827eb47772dfc44dee6aa6f7c
Myoboku <71690611+Myouboku@users.noreply.github.com>
2025-06-13 18:28:08 +0200

    feat: nvim auto dark mode

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -7,9 +7,6 @@ return {
       italic_comments = true,
       borderless_pickers = true,
     },
-    keys = {
-      { "<leader>ut", "<cmd>CyberdreamToggleMode<CR>", desc = "Toggle Light/Dark theme" },
-    },
   },
   {
     "LazyVim/LazyVim",
